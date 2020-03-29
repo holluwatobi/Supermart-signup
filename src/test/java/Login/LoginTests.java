4fbbba5cb2c831a00b7104041e45997fc4906da4
holluwatobi <holluwatobi1@gmail.com>
2020-03-29 19:24:13 +0100

    push first code commit

---
+++ src/test/java/Login/LoginTests.java
@@ -0,0 +1,41 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    private WebDriver driver;
+
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.supermart.ng/");
+        Thread.sleep(5000);
+
+        //1. minimize the window
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+        driver.findElement(By.id("email")).sendKeys("holluwatobi1@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("oluwatobi");
+        driver.findElement(By.id("loginbutton")).click();
+
+    }
+    public static void main(String args[]) throws InterruptedException {
+        LoginTests tests = new LoginTests();
+        tests.setUp();
+
+    }
+}
+
+
+
+
+
+
+
+
